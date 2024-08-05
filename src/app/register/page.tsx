32dbf87cd146532ddb78b1d6a979703315da232c
Meguru <>
2024-08-06 01:12:41 +0900

    registerのcreate an account修正した

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -7,7 +7,7 @@ function Page() {
       <Header />
       <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-xl mb-6 font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                     Create an account
             </h1>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
