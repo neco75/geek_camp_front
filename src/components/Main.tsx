2822b31661553ee3206eb54f6cf1f6d7984ce607
sagara_rikuto <sagara_rikuto@plise.co.jp>
2024-08-10 15:14:40 +0900

    修正

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-
+"use client";
 
 import React, { useState } from "react";
 import Map from "./Map";
@@ -16,32 +16,34 @@ function Main() {
   };
 
   return (
-
-    <div
-      id="main-contents"
-      className="text-center p-5 flex flex-col lg:flex-row items-center justify-center w-full"
-    >
-      <form
+    <>
+      <div
+        id="main-contents"
+        className="text-center p-5 flex flex-col lg:flex-row items-center justify-center w-full"
+      >
+        <form
           className="m-5 w-full max-w-xs flex flex-col justify-center"
           onSubmit={handleSubmit}
         >
-        <div className="mb-5">
-          <p className="text-lg mb-2">あなたの現在地</p>
-          <input
-            type="text"
-            defaultValue="0000000"
-            className="text-center border-2 border-r-2 border-gray-500"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full text-white bg-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          <div className="mb-5">
+            <p className="text-lg mb-2">あなたの現在地</p>
+            <input
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="text-center border-2 border-r-2 border-gray-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full text-white bg-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
             旅行先を決める
           </button>
         </form>
         <div className="m-5 w-full">
           <h3 className="text-lg">Map</h3>
-          <div className="border-2 border-gray-500  overflow-y-scroll">
+          <div className="border-2 border-gray-500 overflow-y-scroll">
             <Map />
           </div>
         </div>
