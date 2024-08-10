aa970f32b51abab0ea92564f2f1d56ac6afc341f
sagara_rikuto <sagara_rikuto@plise.co.jp>
2024-08-10 13:00:29 +0900

    背景とボタンの修正

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Page() {
   return (
     <div>
-      <section className="bg-gray-50 dark:bg-gray-900">
+      <section>
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <a
             href="#"
